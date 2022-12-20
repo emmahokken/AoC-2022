23cb9f0bfdba87067d790f2d4736cf5855f4ac80
Emma Hokken <emma.hokken@politie.nl>
2022-12-20 14:45:37 +0100

    day 20 part 2

--- days/20.py
+++ days/20.py
@@ -25,22 +25,23 @@ def coord_to_move(coords):
 
 def find_zero(coords):
     for i, c in enumerate(coords):
-        if c.value == 0:
+        if c == 0:
             return i
 
-def mix(coords, coords_len):
-    while any_moves_left(coords):
-        # find the first number that has not been moved yet
-        i, c = coord_to_move(coords)
+def mix(coords, orig_coords, coords_len):
+    for c in orig_coords:
+        i = coords.index(c)
+
+        value, idx = c
         # new index
-        new_i = (i + c.value) % (coords_len - 1)
+        new_i = (i + value) % (coords_len - 1)
         if new_i == 0:
             new_i = coords_len - 1
 
-        print(f'We are moving {c.value}, from index {i}, to index {new_i}')
+        # print(f'We are moving {value}, from index {i}, to index {new_i}')
 
-        coords = np.delete(coords, i)
-        coords = np.insert(coords, new_i, Coord(c.value, moved=True))
+        coords.pop(i)
+        coords.insert(new_i, c)
 
     return coords
 
@@ -51,22 +52,30 @@ def main():
     2622 right!
     '''
 
-    coords = np.array([Coord(int(line)) for line in read_file(day=20)])
+    coords = [(int(line),i) for i, line in enumerate(read_file(day=20))]
+    orig_coords = [(int(line),i) for i, line in enumerate(read_file(day=20))]
     coords_len = len(coords)
-    coords = mix(coords, coords_len)
-
+    coords = mix(coords, orig_coords, coords_len)
+    mixed_coords = [c[0] for c in coords]
     # find 0 index
-    z_idx = find_zero(coords)
+    z_idx = mixed_coords.index(0)
 
     # now find 1000th, 2000th, and 3000th value after 0
-    ctw = [coords[(z_idx + i) % coords_len].value for i in range(1000, 3001, 1000)]
+    ctw = [mixed_coords[(z_idx + i) % coords_len] for i in range(1000, 3001, 1000)]
 
     print(f'Part 1: the sum of the 1000th, 2000th, and 3000th numbers after mixing is {sum(ctw)}')
 
     encryption_key = 811589153
+    coords = [(int(line) * encryption_key, i) for i, line in enumerate(read_file(day=20))]
+    orig_coords = [(int(line) * encryption_key, i) for i, line in enumerate(read_file(day=20))]
 
-    coords = np.array([Coord(int(line)*encryption_key) for line in read_file(day=20)])
+    for _ in range(10):
+        coords = mix(coords, orig_coords, coords_len)
 
+    mixed_coords = [c[0] for c in coords]
+    z_idx = mixed_coords.index(0)
+    ctw = [mixed_coords[(z_idx + i) % coords_len] for i in range(1000, 3001, 1000)]
+    print(f'Part 2: the sum of the 1000th, 2000th, and 3000th numbers after mixing is {sum(ctw)}')
 
 if __name__ == '__main__':
     main()
